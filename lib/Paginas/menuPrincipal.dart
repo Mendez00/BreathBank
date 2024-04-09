6a5a08ebe9b77992c2860e84f2079dc737c1985a
Mendez00 <xma1001@alu.ubu.es>
2024-04-09 20:53:34 +0200

    Menú de la aplicación.
    Añadimos el menú definitivo de la app.

--- lib/Paginas/menuPrincipal.dart
+++ lib/Paginas/menuPrincipal.dart
@@ -1,79 +1,208 @@
+import 'package:breath_bank/Paginas/ejerciciosIniciales.dart';
+import 'package:breath_bank/Paginas/inicioSesion.dart';
 import 'package:breath_bank/Paginas/perfil.dart';
 import 'package:breath_bank/Paginas/estadisticas.dart';
 import 'package:breath_bank/Paginas/ajustes.dart';
 import 'package:breath_bank/Paginas/amigos.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-class menuPrincipal extends StatefulWidget {
-  menuPrincipal({super.key});
+class MenuPrincipal extends StatefulWidget {
+  final Function()? onTap;
+  const MenuPrincipal({super.key, required this.onTap});
 
   @override
-  State<menuPrincipal> createState() => estadoMenuPrincipal();
+  State<MenuPrincipal> createState() => EstadoMenuPrincipal();
 }
 
-class estadoMenuPrincipal extends State<menuPrincipal> {
+class EstadoMenuPrincipal extends State<MenuPrincipal> {
+  int indiceActual = 1;
 
-  int indiceActual = 0;
-
-  void barraNavegacion(int indice) {
-    setState(() {
-      indiceActual = indice;
-    });
-  }
-
-  final List paginas = [
-    //estadisticas
-    Estadisticas(),
-    //perfil
-    Perfil(),
-    //ajustes
-    Ajustes(),
-    //amigos
-    Amigos(),
+  final List<Widget> pantallas = [
+    const Estadisticas(),
+    const Text(''),
+    const Amigos(),
   ];
 
+  void cerrarSesion() async {
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "BreathBank",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 30,
-          ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            Image.asset(
+              'lib/images/logo.png',
+              width: 200,
+              height: 200,
+              fit: BoxFit.contain,
+            ),
+            SizedBox(height: 30),
+            Expanded(
+              child: pantallas[indiceActual],
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                  bottom: 200), // Ajusta el margen inferior según necesites
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            EjerciciosIniciales(onTap: () {})),
+                  );
+                },
+                child: Container(
+                  width: 250,
+                  height: 100,
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.lightBlueAccent,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: const Center(
+                    child: Text(
+                      "Realizar Inversión",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        backgroundColor: Colors.lightBlueAccent,
       ),
-      body: paginas[indiceActual],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.lightBlueAccent,
-        currentIndex: indiceActual,
-        onTap: barraNavegacion,
-        items: const [
-          //estadisticas
-          BottomNavigationBarItem(
-            icon: Icon(Icons.analytics),
-            label: 'Estadisticas',
-          ),
 
-          //perfil
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Perfil',
-          ),
 
-          //Ajustes
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Ajustes',
-          ),
+      appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: Builder(
+            builder: (context) => IconButton(
+              icon: const Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            ),
+          )),
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                DrawerHeader(child: Image.asset('lib/images/logo.png')),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                ),
+                ListTile(
+                  leading: const Icon(
+                    Icons.home,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  title: const Text(
+                    'Home',
+                    style: TextStyle(color: Colors.lightBlueAccent),
+                  ),
+                  onTap: () {
+                    // Navegar a la página "Home" cuando se toca el ListTile
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MenuPrincipal(
+                                onTap: () {},
+                              )),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(
+                    Icons.analytics,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  title: const Text(
+                    'Estadisticas',
+                    style: TextStyle(color: Colors.lightBlueAccent),
+                  ),
+                  onTap: () {
+                    // Navegar a la página "Perfil" cuando se toca el ListTile
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Estadisticas( )
+                      ),
+                    );
+                  },
+                ),
 
-          //Amigos
-          BottomNavigationBarItem(
-            icon: Icon(Icons.group),
-            label: 'Amigos',
-          ),
-        ],
+                ListTile(
+                  leading: const Icon(
+                    Icons.person,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  title: const Text(
+                    'Perfil',
+                    style: TextStyle(color: Colors.lightBlueAccent),
+                  ),
+                  onTap: () {
+                    // Navegar a la página "Perfil" cuando se toca el ListTile
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Perfil(
+                            onTap: () {},
+                          )),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(
+                    Icons.settings,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  title: const Text(
+                    'Ajustes',
+                    style: TextStyle(color: Colors.lightBlueAccent),
+                  ),
+                  onTap: () {
+                    // Navegar a la página "Ajustes" cuando se toca el ListTile
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Ajustes(
+                                onTap: () {},
+                              )),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(
+                    Icons.logout,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  title: const Text(
+                    'Cerrar sesión',
+                    style: TextStyle(color: Colors.lightBlueAccent),
+                  ),
+                  onTap: () {
+                    cerrarSesion();
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
