012df0fd7dc78858dcea06bca6f366985ed06f77
Mendez00 <xma1001@alu.ubu.es>
2024-04-30 08:22:50 +0200

    Drawer definitivo.
    Se añade el drawer a todas las páginas, pequeñas modificaciones en menuPrincipal.

---
+++ lib/Paginas/drawer.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:breath_bank/Paginas/ajustes.dart';
+import 'package:breath_bank/Paginas/inicioSesion.dart';
+import 'package:breath_bank/Paginas/menuPrincipal.dart';
+import 'package:breath_bank/Paginas/estadisticas.dart';
+import 'package:breath_bank/Paginas/perfil.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    void IrAlInicioSesion(BuildContext context) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => InicioSesion()),
+      );
+    }
+
+    void cerrarSesion(BuildContext context) async {
+      FirebaseAuth.instance.signOut();
+      IrAlInicioSesion(context);
+    }
+
+    return Drawer(
+        child: ListView(
+      padding: EdgeInsets.zero,
+      children: [
+        DrawerHeader(
+          child: Image.asset('lib/images/logo.png'),
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.home,
+            color: Colors.lightBlueAccent,
+          ),
+          title: Text(
+            'Home',
+            style: TextStyle(color: Colors.lightBlueAccent),
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MenuPrincipal(onTap: () {}),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.analytics,
+            color: Colors.lightBlueAccent,
+          ),
+          title: Text(
+            'Estadisticas',
+            style: TextStyle(color: Colors.lightBlueAccent),
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Estadisticas(),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.person,
+            color: Colors.lightBlueAccent,
+          ),
+          title: Text(
+            'Perfil',
+            style: TextStyle(color: Colors.lightBlueAccent),
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Perfil(onTap: () {}),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.settings,
+            color: Colors.lightBlueAccent,
+          ),
+          title: Text(
+            'Ajustes',
+            style: TextStyle(color: Colors.lightBlueAccent),
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Ajustes(onTap: () {}),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.logout,
+            color: Colors.lightBlueAccent,
+          ),
+          title: Text(
+            'Cerrar sesión',
+            style: TextStyle(color: Colors.lightBlueAccent),
+          ),
+          onTap: () {
+            cerrarSesion(context);
+          },
+        ),
+      ],
+    ));
+  }
+}
--- lib/Paginas/menuPrincipal.dart
+++ lib/Paginas/menuPrincipal.dart
@@ -1,11 +1,11 @@
-import 'package:breath_bank/Paginas/ejerciciosIniciales.dart';
+import 'package:breath_bank/Paginas/inversion.dart';
 import 'package:breath_bank/Paginas/inicioSesion.dart';
+import 'package:breath_bank/Paginas/drawer.dart';
 import 'package:breath_bank/Paginas/perfil.dart';
 import 'package:breath_bank/Paginas/estadisticas.dart';
 import 'package:breath_bank/Paginas/ajustes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MenuPrincipal extends StatefulWidget {
   final Function()? onTap;
@@ -16,181 +16,93 @@ class MenuPrincipal extends StatefulWidget {
 }
 
 class EstadoMenuPrincipal extends State<MenuPrincipal> {
-  void IrAlInicioSesion(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => InicioSesion()),
-    );
-  }
-
-  void cerrarSesion() async {
-    FirebaseAuth.instance.signOut();
-    IrAlInicioSesion(context);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 20),
-            Image.asset(
-              'lib/images/logo.png',
-              width: 200,
-              height: 200,
-              fit: BoxFit.contain,
-            ),
-            SizedBox(height: 30),
-            Container(
-              margin: EdgeInsets.only(bottom: 200),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            EjerciciosIniciales(onTap: () {})),
-                  );
+        backgroundColor: Colors.white,
+        drawer: MyDrawer(),
+        appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: Builder(
+              builder: (context) => IconButton(
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
                 },
-                child: Container(
-                  width: 250,
-                  height: 100,
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      color: Colors.lightBlueAccent,
-                      borderRadius: BorderRadius.circular(8)),
-                  child: const Center(
-                    child: Text(
-                      "Realizar Inversión",
-                      style: TextStyle(color: Colors.white),
+              ),
+            )),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20),
+                Image.asset(
+                  'lib/images/logo.png',
+                  width: 175,
+                  height: 175,
+                  fit: BoxFit.contain,
+                ),
+                const SizedBox(height: 75),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'MEJORA TU VIDA\n',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text:
+                              'Realizar ejercicios diarios para mejorar tu capacidad pulmonar, tu estrés y tu concentración en solo 10 minutos',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Container(
+                  margin: EdgeInsets.only(bottom: 200),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Inversion(onTap: () {})),
+                      );
+                    },
+                    child: Container(
+                      width: 250,
+                      height: 100,
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: Colors.lightBlueAccent,
+                          borderRadius: BorderRadius.circular(8)),
+                      child: const Center(
+                        child: Text(
+                          "Realizar Inversión",
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          leading: Builder(
-            builder: (context) => IconButton(
-              icon: const Icon(
-                Icons.menu,
-                color: Colors.black,
-              ),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-            ),
-          )),
-      drawer: Drawer(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                DrawerHeader(child: Image.asset('lib/images/logo.png')),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 25.0),
-                ),
-                ListTile(
-                  leading: const Icon(
-                    Icons.home,
-                    color: Colors.lightBlueAccent,
-                  ),
-                  title: const Text(
-                    'Home',
-                    style: TextStyle(color: Colors.lightBlueAccent),
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => MenuPrincipal(
-                                onTap: () {},
-                              )),
-                    );
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(
-                    Icons.analytics,
-                    color: Colors.lightBlueAccent,
-                  ),
-                  title: const Text(
-                    'Estadisticas',
-                    style: TextStyle(color: Colors.lightBlueAccent),
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const Estadisticas()),
-                    );
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(
-                    Icons.person,
-                    color: Colors.lightBlueAccent,
-                  ),
-                  title: const Text(
-                    'Perfil',
-                    style: TextStyle(color: Colors.lightBlueAccent),
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => Perfil(
-                                onTap: () {},
-                              )),
-                    );
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(
-                    Icons.settings,
-                    color: Colors.lightBlueAccent,
-                  ),
-                  title: const Text(
-                    'Ajustes',
-                    style: TextStyle(color: Colors.lightBlueAccent),
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => Ajustes(
-                                onTap: () {},
-                              )),
-                    );
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(
-                    Icons.logout,
-                    color: Colors.lightBlueAccent,
-                  ),
-                  title: const Text(
-                    'Cerrar sesión',
-                    style: TextStyle(color: Colors.lightBlueAccent),
-                  ),
-                  onTap: () {
-                    cerrarSesion();
-                  },
-                ),
               ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
